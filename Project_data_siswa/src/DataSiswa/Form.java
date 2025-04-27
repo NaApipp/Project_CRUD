2eed7c0de61ff87857cf855a7c3980cdda052162
Nabil Apipp <123299418+NaApipp@users.noreply.github.com>
2025-04-27 11:20:15 +0700

    Update Script Form.java

--- Project_data_siswa/src/DataSiswa/Form.java
+++ Project_data_siswa/src/DataSiswa/Form.java
@@ -1,6 +1,6 @@
-/*Package Name*/
+
 package DataSiswa;
-/*Untuk Memanggil beberapa variable dari Library "JDBC MySql Connection"*/
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,9 +10,8 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-/*Nama Class*/
+
 public class Form extends javax.swing.JFrame {
-    /*Variable Connect To Database*/
 public Connection con;
 public Statement st;
 public ResultSet rs;
@@ -20,33 +19,33 @@ public DefaultTableModel model;
     /**
      * Creates new form Form
      */
-    public Form() {
+    public Form() { 
         initComponents();
-        String[] header = {"NIPD", "NAMA", "JURUSAN", "ALAMAT"};
-        model = new DefaultTableModel (header, 0);
-        tabel.setModel(model);
+        String[] header = {"NIPD", "NAMA", "JURUSAN", "ALAMAT"}; //daftar nama kolom pada table db
+        model = new DefaultTableModel (header, 0); //membuat model table terbaru sesuai header dimulai dari 0
+        tabel.setModel(model); //Menghubungkan model yang baru dibuat tadi ke komponen tabel
         insert();
     }
-    /*Syntax Untuk Menampilkan data ke dalam tabel*/
+    
     public void insert() {
-        KoneksiDatabase kon = new KoneksiDatabase();
+        KoneksiDatabase kon = new KoneksiDatabase(); 
         try {
-            con = KoneksiDatabase.getKoneksi();
-            st = con.createStatement();
-            rs = st.executeQuery("SELECT * FROM tbl_data2");
-            while(rs.next()) {
-                String[] row = {rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)};
-                model.addRow(row);
+            con = KoneksiDatabase.getKoneksi(); //Membuat objek baru  (con) dari class KoneksiDatabase, Mengambil koneksi ke database menggunakan getKoneksi().
+            st = con.createStatement(); //membuat statement sql untuk menjalankan query
+            rs = st.executeQuery("SELECT * FROM tbl_data2");  //Menjalankan query SELECT semua data dari tabel tbl_data2.   
+            while(rs.next()) { 
+                String[] row = {rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)}; //➡Ambil isi kolom ke-1, 2, 3, 4 dari baris saat ini di ResultSet, lalu taruh ke array row.
+                model.addRow(row); //Menambahkan array row tadi ke tabel model (DefaultTableModel)
             }
         tabel.setModel(model);
-    } catch(SQLException e) {
+    } catch(SQLException e) { //Kalau terjadi error SQL (contohnya koneksi gagal, query salah),
             System.out.println("gagal " +e.getMessage());
 }}
 
 
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -66,6 +65,7 @@ public DefaultTableModel model;
         btnPreview = new java.awt.Button();
         jScrollPane2 = new javax.swing.JScrollPane();
         tabel = new javax.swing.JTable();
+        jLabel6 = new javax.swing.JLabel();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -169,9 +169,6 @@ public DefaultTableModel model;
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(153, 153, 153)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
                         .addGap(22, 22, 22)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addGroup(layout.createSequentialGroup()
@@ -196,19 +193,27 @@ public DefaultTableModel model;
                                     .addComponent(input_alamat, javax.swing.GroupLayout.DEFAULT_SIZE, 215, Short.MAX_VALUE)))))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(153, 153, 153)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(163, 163, 163)
+                        .addComponent(jLabel6)))
                 .addContainerGap(15, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(29, 29, 29)
+                        .addGap(24, 24, 24)
                         .addComponent(jLabel2))
-                    .addComponent(input_nipd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
+                        .addComponent(input_nipd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(input_nama, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -227,149 +232,130 @@ public DefaultTableModel model;
                     .addComponent(btnChange, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnPreview, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 402, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(43, 43, 43)
+                .addComponent(jLabel6))
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
 
-    private void input_nipdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_input_nipdActionPerformed
+    private void input_nipdActionPerformed(java.awt.event.ActionEvent evt) {                                           
         // TODO add your handling code here:
-    }//GEN-LAST:event_input_nipdActionPerformed
+    }                                          
 
-    private void input_namaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_input_namaActionPerformed
+    private void input_namaActionPerformed(java.awt.event.ActionEvent evt) {                                           
         // TODO add your handling code here:
-    }//GEN-LAST:event_input_namaActionPerformed
+    }                                          
 
-    private void input_alamatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_input_alamatActionPerformed
+    private void input_alamatActionPerformed(java.awt.event.ActionEvent evt) {                                             
         // TODO add your handling code here:
-    }//GEN-LAST:event_input_alamatActionPerformed
+    }                                            
 
-    private void input_jurusanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_input_jurusanActionPerformed
+    private void input_jurusanActionPerformed(java.awt.event.ActionEvent evt) {                                              
         // TODO add your handling code here:
-    }//GEN-LAST:event_input_jurusanActionPerformed
+    }                                             
 
-
-    /*Syntax untuk button tambah / add*/
-    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {                                       
         // TODO add your handling code here:
         
         
          try{
-             /*Untuk memilih database mana yang akan di gunakan*/
-            String url="jdbc:mysql://localhost:3306/db_siswa";
-            String user="root";
-            String pass="";
-            String query = "INSERT INTO tbl_data2 VALUES (?, ?, ?, ?)";
-            Class.forName("com.mysql.jdbc.Driver");
+            String url="jdbc:mysql://localhost:3306/db_siswa"; //koneksi ke database
+            String user="root"; //username
+            String pass=""; //password
+            String query = "INSERT INTO tbl_data2 VALUES (?, ?, ?, ?)"; //membuat perintah untuk memasukkan data ke tabel tbl_data2,dengan 4 data yang nanti diisi menggunakan PreparedStatement supaya aman dan terhindar dari SQL Injection.
+            Class.forName("com.mysql.jdbc.Driver"); //Memuat/mendaftarkan driver JDBC MySQL ke program Java, supaya Java bisa terhubung ke database MySQL.
             
-            con=DriverManager.getConnection(url, user, pass);
-            st=con.createStatement();
+            con=DriverManager.getConnection(url, user, pass); //membuka koneksi ke database MySQL, dengan: url, username, dan password
+            st=con.createStatement(); //Membuat Statement SQL dari koneksi con tadi, supaya kamu bisa mengirim query SQL ke database.
             
-            PreparedStatement sta = con.prepareStatement(query);
-            sta.setString(1, input_nipd.getText());
-            sta.setString(2, input_nama.getText());
-            sta.setString(3, input_jurusan.getText());
-            sta.setString(4, input_alamat.getText());
-             sta.execute();
+            PreparedStatement sta = con.prepareStatement(query); ///mempersiapkan query sql agar bisa di isi 1/1
+            sta.setString(1, input_nipd.getText()); //mengisi nilai "?" di query berdasarkan input dari form
+            sta.setString(2, input_nama.getText()); //mengisi nilai "?" di query berdasarkan input dari form
+            sta.setString(3, input_jurusan.getText()); //mengisi nilai "?" di query berdasarkan input dari form
+            sta.setString(4, input_alamat.getText()); //mengisi nilai "?" di query berdasarkan input dari form
+             sta.execute(); //Menjalankan query tersebut ke database. (data baru di tambahkan ke tabel)
              
-             JOptionPane.showMessageDialog(null, "berhasil");
+             JOptionPane.showMessageDialog(null, "Data berhasil Diinputkan"); //mssg jika data acc ke database
         }catch(Exception e){
-            System.out.println("gagal "+e.getMessage());
+            System.out.println("gagal "+e.getMessage()); //mssg jika data gagal ke database
         }
-         /*Update Tabel Secara Otomatis*/
+         //UPDATE TABEL AUTOMATIC (Mengupdate tabel secara otomatis apa bila ada perubahan
           String[] header = {"NIPD", "NAMA", "JURUSAN", "ALAMAT"};
         model = new DefaultTableModel (header, 0);
         tabel.setModel(model);
         insert();
-    }//GEN-LAST:event_btnAddActionPerformed
+    }                                      
 
-
-
-    /*Syntax Untuk Button Preview*/
-    private void btnPreviewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPreviewActionPerformed
-        // TODO add your handling code here:
-        
+    private void btnPreviewActionPerformed(java.awt.event.ActionEvent evt) {                                           
+        //UPDATE TABEL AUTOMATIC (Mengupdate tabel secara otomatis apa bila ada perubahan
         String[] header = {"NIPD", "NAMA", "JURUSAN", "ALAMAT"};
         model = new DefaultTableModel (header, 0);
         tabel.setModel(model);
         insert();
-    }//GEN-LAST:event_btnPreviewActionPerformed
+    }                                          
 
-
-
-    /*Syntax untuk tabel (yg berfungsi untuk ketika kita klik data yang ada di tabel dapat muncul pada kolom TextField)*/
-    private void tabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabelMouseClicked
+    private void tabelMouseClicked(java.awt.event.MouseEvent evt) {                                   
         // TODO add your handling code here:
         
-        int baris = tabel.rowAtPoint(evt.getPoint());
+        int baris = tabel.rowAtPoint(evt.getPoint()); //untuk menentukan baris keberapa yang diklik di tabel
         
         String nipd = tabel.getValueAt(baris, 0).toString();
-        input_nipd.setText(nipd);
+        input_nipd.setText(nipd); //Ambil nilai kolom ke-0 (kolom pertama, NIPD) dari baris yang diklik, lalu tampilkan ke field input_nipd.
         
         String nama = tabel.getValueAt(baris, 1).toString();
-        input_nama.setText(nama);
+        input_nama.setText(nama); //Ambil nilai kolom ke-1 (NAMA) dari baris yang diklik, lalu tampilkan ke field input_nama.
         
         String jurusan = tabel.getValueAt(baris, 2).toString();
-        input_jurusan.setText(jurusan);
+        input_jurusan.setText(jurusan); //Ambil nilai kolom ke-2 (JURUSAN) dari baris yang diklik, lalu tampilkan ke field input_jurusan.
         
         String alamat = tabel.getValueAt(baris, 3).toString();
-        input_alamat.setText(alamat);
-    }//GEN-LAST:event_tabelMouseClicked
+        input_alamat.setText(alamat); ////Ambil nilai kolom ke-3 (ALAMAT) dari baris yang diklik, lalu tampilkan ke field input_alamat.
+    }                                  
 
-
-
-
-
-    /*Syntax Button Change / Update*/
-    private void btnChangeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnChangeActionPerformed
+    private void btnChangeActionPerformed(java.awt.event.ActionEvent evt) {                                          
         // TODO add your handling code here:
-        con = KoneksiDatabase.getKoneksi();
+        con = KoneksiDatabase.getKoneksi(); //Membuat objek baru  (con) dari class KoneksiDatabase, Mengambil koneksi ke database menggunakan getKoneksi().
         
         try {
-            String sql = "UPDATE tbl_data2 SET nama='" +input_nama.getText()+
-                    "',jurusan ='" +input_jurusan.getText()+
-                    "',alamat ='" +input_alamat.getText()+"'"+
-                    "WHERE nipd ='" +input_nipd.getText()+"'";
+            String sql = "UPDATE tbl_data2 SET nama='" +input_nama.getText()+ //Membuat query SQL untuk mengupdate data di tabel tbl_data2
+                    "',jurusan ='" +input_jurusan.getText()+ //jurusan dengan nilai dari input_jurusan
+                    "',alamat ='" +input_alamat.getText()+"'"+ //alamat dengan nilai dari input_alamat
+                    "WHERE nipd ='" +input_nipd.getText()+"'"; //Tapi baris yang nipd-nya sama dengan nilai dari input_nipd.
             
             PreparedStatement sta = con.prepareStatement(sql);
             sta.execute();
-            JOptionPane.showMessageDialog(null, "Berhasil Di Edit");
+            JOptionPane.showMessageDialog(null, "Data Berhasil Di Edit");  //mssg jika data berhasil di edit
         } catch (Exception e) {
-          JOptionPane.showMessageDialog(null, "gagal di edit" +e.getMessage());
+          JOptionPane.showMessageDialog(null, "Data Gagal Di Edit" +e.getMessage()); //mssg jika data gagal di edit
         }
-        /*Update Tabel Secara Otomatis*/
+        //UPDATE TABEL AUTOMATIC (Mengupdate tabel secara otomatis apa bila ada perubahan
          String[] header = {"NIPD", "NAMA", "JURUSAN", "ALAMAT"};
         model = new DefaultTableModel (header, 0);
         tabel.setModel(model);
         insert();
-    }//GEN-LAST:event_btnChangeActionPerformed
+    }                                         
 
-
-
-
-
-    /*Syntax untuk Button Delete*/
-    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {                                          
         // TODO add your handling code here:
          con = KoneksiDatabase.getKoneksi();
 try {
-            String sql = "DELETE FROM tbl_data2 WHERE nipd  ='" +input_nipd.getText() + "'";
+            String sql = "DELETE FROM tbl_data2 WHERE nipd  ='" +input_nipd.getText() + "'"; //query sql delete data berdasarkan nipd
             
         PreparedStatement sta = con.prepareStatement(sql);
         sta.execute();
-        JOptionPane.showMessageDialog(null, "Berhasil menghapus Brokk");
+        JOptionPane.showMessageDialog(null, "Data Berhasil dihapus"); //mssg jika data berhasil di hapus
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "gagal hapus brokk" + e.getMessage());
+            JOptionPane.showMessageDialog(null, "Data gagal dihapus" + e.getMessage()); //mssg jika data gagal di hapus
         }
-        //script update tabel
+        //UPDATE TABEL AUTOMATIC (Mengupdate tabel secara otomatis apa bila ada perubahan
        String[] header = {"NIPD","NAMA","JURUSAN","ALAMAT"};
         model=new DefaultTableModel(header, 0);
         tabel.setModel(model);
         insert();
-    }//GEN-LAST:event_btnDeleteActionPerformed
+    }                                         
 
     /**
      * @param args the command line arguments
@@ -406,7 +392,7 @@ try {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private java.awt.Button btnAdd;
     private java.awt.Button btnChange;
     private java.awt.Button btnDelete;
@@ -420,9 +406,10 @@ try {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable tabel;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
